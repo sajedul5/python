81fa95696f0ebae12af3ca0d5de10188d036607d
sajedul5 <sajedul.idb@gmail.com>
2022-10-31 22:10:05 +0600

    31 oct

---
+++ basic/input.py
@@ -0,0 +1,7 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
+age = age + 1
+print("Hello "+name)
+print("I am "+str(age)+" years old")
+print("I am "+str(height)+"cm tall")
