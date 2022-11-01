31377461c6137a668323eb39ff8ed2a3eaacb215
sajedul5 <sajedul.idb@gmail.com>
2022-11-01 21:45:58 +0600

    1 November

---
+++ basic/logical_operators.py
@@ -0,0 +1,11 @@
+# logical operators (and,or,not) = used to check if two or more conditional statements
+
+temp = int(input("What is the temperature outside?: "))
+
+if temp >= 0 and temp <= 30:
+    print("The temperature is good today!")
+    print("Go outside!")
+elif temp < 0 or temp > 30:
+    print("The temperature is bad today!")
+    print("Stay inside!")
+
