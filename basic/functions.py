44452571e26873978e4a3630a57dc122f0927564
sajedul5 <sajedul.idb@gmail.com>
2023-01-09 20:57:13 +0600

    string format, random number, exception handling & file detection

--- basic/functions.py
+++
@@ -1,8 +0,0 @@
-# function = a block of code which is executed only when it is called.
-
-def hello(first_name,last_name,age):
-    print("Hello"+" "+first_name+" "+last_name)
-    print("You are "+str(age)+" years old")
-    print("Have a nice day!")
-
-hello("SI","Shakil",28)
--- basic/input.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name?: ")
-age = int(input("How old are you?: "))
-height = float(input("How tall are you?: "))
-age = age + 1
-print("Hello "+name)
-print("I am "+str(age)+" years old")
-print("I am "+str(height)+"cm tall")
